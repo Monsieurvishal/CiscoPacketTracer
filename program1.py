014680d4b5e0f029d2e0b748330b8522e79e1273
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-04-03 19:58:39 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 19:51:15 2020
+
+@author: Vishal
+"""
+
+import cv2
+
+img = cv2.imread('watch.jpg',0)
+cv2.imshow('image',img)
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv2.imwrite('watchgray.png',img)
+    cv2.destroyAllWindows()
