b9fc3eb578933b22400fdde2f28e2092c4529137
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-04-04 00:40:32 +0530

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 23:05:43 2020
+
+@author: Vishal
+"""
+import cv2
+
+cap = cv2.VideoCapture('vtest.avi')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
